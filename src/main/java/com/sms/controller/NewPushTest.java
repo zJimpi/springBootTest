858d480c26c6e038e581b7afe36acfd0120b26d3
zJimpi <jimpidesk@gmail.com>
2023-08-28 20:02:59 +0530

    2nd push test after 1st pull

---
+++ src/main/java/com/sms/controller/NewPushTest.java
@@ -0,0 +1,10 @@
+package com.sms.controller;
+
+public class NewPushTest {
+
+	void testPush1()
+	{
+		String msg2 = "icant see teh file in previous pull";
+
+	}
+}
--- src/main/java/com/sms/controller/TestGithub.java
+++ src/main/java/com/sms/controller/TestGithub.java
@@ -1,6 +1,8 @@
+package com.sms.controller;
 class TestGithub
   {
     public void test()
     {
       String msg = "yoo!";
+    }
   }
